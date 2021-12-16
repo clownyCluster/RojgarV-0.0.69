014727a0cc0e7d85067b3482a3925ec589e2f028
clownyCluster <pokharelrupace@gmail.com>
2021-12-16 14:32:09 +0545

    Can set origin and destination on map

--- lib/screens/maps.dart
+++ lib/screens/maps.dart
@@ -8,6 +8,22 @@ class GoogleMaps extends StatefulWidget {
 }
 
 class _GoogleMapsState extends State<GoogleMaps> {
+  static const _initialCameraPosition = CameraPosition(
+    target: LatLng(27.684893, 85.346375),
+    zoom: 16.5,
+    tilt: 50,
+  );
+
+  GoogleMapController _googleMapController;
+  Marker _origin;
+  Marker _destination;
+
+  @override
+  void dispose() {
+    _googleMapController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,13 +37,86 @@ class _GoogleMapsState extends State<GoogleMaps> {
                 end: Alignment.bottomRight),
           ),
         ),
+        actions: [
+          if(_origin != null)
+          TextButton(
+            onPressed: () => _googleMapController.animateCamera(
+              CameraUpdate.newCameraPosition(
+                CameraPosition(
+                  target: _origin.position,
+                  zoom: 16.5,
+                  tilt: 70.0,
+                ),
+              ),
+            ),
+            style: TextButton.styleFrom(
+              primary: Colors.green,
+              textStyle: const TextStyle(fontWeight: FontWeight.w600),
+            ),
+            child: const Text('ORIGIN'),
+          ),
+          if(_destination != null)
+          TextButton(
+            onPressed: () => _googleMapController.animateCamera(
+              CameraUpdate.newCameraPosition(
+                CameraPosition(
+                  target: _destination.position,
+                  zoom: 16.5,
+                  tilt: 70.0,
+                ),
+              ),
+            ),
+            style: TextButton.styleFrom(
+              primary: Colors.blue,
+              textStyle: const TextStyle(fontWeight: FontWeight.w600),
+            ),
+            child: const Text('DEST'),
+          ),
+        ],
       ),
       body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: LatLng(27.684778, 85.346499),
-          zoom: 15,
-        ),
+        initialCameraPosition: _initialCameraPosition,
+        onMapCreated: (controller) => _googleMapController = controller,
+        markers: {
+          if (_origin != null) _origin,
+          if (_destination != null) _destination,
+        },
+        onLongPress: _addMarker,
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        child: Icon(Icons.location_searching),
+        onPressed: () => _googleMapController.animateCamera(
+            CameraUpdate.newCameraPosition(_initialCameraPosition)),
       ),
     );
+  }
+
+  void _addMarker(LatLng pos) {
+    if (_origin == null || (_origin != null && _destination != null)) {
+      setState(() {
+        _origin = Marker(
+          markerId: const MarkerId('origin'),
+          infoWindow: const InfoWindow(title: 'Origin'),
+          icon:
+              BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+          position: pos,
+        );
+        // Reset Destination
+        _destination = null;
+      });
+    } else {
+      setState(() {
+        _destination = Marker(
+          markerId: const MarkerId('destination'),
+          infoWindow: const InfoWindow(title: 'Destination'),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+          position: pos,
+        );
+        // Reset Destination
+        // _destination = null;
+      });
+    }
   }
 }
